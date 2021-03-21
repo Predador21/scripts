4b9da6c05eeb0f6f1f78c11817b8f6b44b5a1152
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-03-21 13:12:03 -0300

    Create HeartBrait.sh

---
+++ HeartBrait.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo /tmp/tmp*/ | xargs -n 1 cp -v /home/g0803211625/credentials.db
+
+gcloud config set account g1003211329@gmail.com
+
+while true
+do
+gcloud cloud-shell ssh --command="date" --authorize-session --force-key-file-overwrite --quiet >> log
+sleep 60
+done
