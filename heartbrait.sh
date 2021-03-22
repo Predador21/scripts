a05ad78e649bf270df3d93977cbcd215dd13bcdb
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-03-22 20:22:38 -0300

    Update heartbrait.sh

--- heartbrait.sh
+++ heartbrait.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+session=$1
+
+user=${session/@*/}
+
+echo "$(pwd)" > home.file
+
 sudo rm -rf echo /tmp/tmp*/credentials.db | xargs -n 1
 
 gcloud info
 
-echo /tmp/tmp*/ | xargs -n 1 cp -v /home/g1003211329/credentials.db
+echo /tmp/tmp*/ | xargs -n 1 cp -v /home/$user/credentials.db
 
 gcloud auth list
 
-gcloud config set account g1203211109@gmail.com
-
 while true
 do
-gcloud cloud-shell ssh --command="date >> log" --authorize-session --force-key-file-overwrite --quiet
+gcloud cloud-shell ssh --command="date >> log" --authorize-session --force-key-file-overwrite --account=$session --quiet
+curl https://g11977765505.000webhostapp.com/ping.php?user=$session
 sleep 60
 done
