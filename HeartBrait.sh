8e96def2201555fed28051e7f915316600d0ed1c
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-03-22 00:27:44 -0300

    Update HeartBrait.sh

--- HeartBrait.sh
+++ HeartBrait.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+#rm -rf log
+
+sudo rm -rf echo /tmp/tmp*/credentials.db | xargs -n 1
+
 gcloud info
 
 echo /tmp/tmp*/ | xargs -n 1 cp -v /home/g0803211625/credentials.db
+
+gcloud auth list
 
 gcloud config set account g1003211329@gmail.com
 
