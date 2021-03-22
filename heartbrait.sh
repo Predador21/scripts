4d072a72339ee6b0e1da70be6a7b9a7a199bfe1b
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-03-22 12:24:04 -0300

    Update and rename HeartBrait.sh to heartbrait.sh

--- heartbrait.sh
+++ heartbrait.sh
@@ -6,11 +6,11 @@ sudo rm -rf echo /tmp/tmp*/credentials.db | xargs -n 1
 
 gcloud info
 
-echo /tmp/tmp*/ | xargs -n 1 cp -v /home/g0803211625/credentials.db
+echo /tmp/tmp*/ | xargs -n 1 cp -v /home/g1003211329/credentials.db
 
 gcloud auth list
 
-gcloud config set account g1003211329@gmail.com
+gcloud config set account g1203211109@gmail.com
 
 while true
 do
