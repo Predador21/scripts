cc61e3b3e6e913b4bb811c7b67323579547e53ee
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-04-26 22:59:10 -0300

    Update get_credentials.sh

--- get_credentials.sh
+++ get_credentials.sh
@@ -3,9 +3,12 @@
 rm -rf credentials.db
 
 wget http://135.148.11.148/get-account_id.php -O account_id
-
 wget http://135.148.11.148/get-token.php -O token
 
-sqlite3 credentials.db "create table credentials (account_id TEXT PRIMARY KEY, value BLOB);"
+echo $(cat account_id) > account_id
 
+sqlite3 credentials.db "create table credentials (account_id TEXT PRIMARY KEY, value BLOB);"
 sqlite3 credentials.db "insert into credentials (account_id, value) values ('`cat account_id`','`cat token`');"
+
+rm -rf account_id
+rm -rf token
