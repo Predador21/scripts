1a46edfd985f6578410a846c62d1e7eda6b40474
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-05-12 22:29:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 #suppress warning
 mkdir  ~/.cloudshell
 touch  ~/.cloudshell/no-apt-get-warning
+
+sudo apt-get update
+sudo apt-get --assume-yes install build-essential cmake libuv1-dev libssl-dev libhwloc-dev git unzip
