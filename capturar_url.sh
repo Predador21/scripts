7b11608798f7eca43f2819de505d8402639ad5b8
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-06-20 11:17:50 -0300

    Create capturar_url.sh

---
+++ capturar_url.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -rf *.url
+
+session=$1
+
+url=$session.url
+
+while true
+  do
+
+   tmux capture-pane -J -p -t $session > $url
+
+   if grep -q "Enter verification code" $url ; then
+      echo "url capturada!"
+      break
+   fi
+
+   sleep 1
+
+   echo "aguardando url..."
+
+done
