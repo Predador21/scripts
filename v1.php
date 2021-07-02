6b47c5c7e31ed6356944263731fd404635e1a0e2
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-07-02 01:26:31 -0300

    Update v1.php

--- v1.php
+++ v1.php
@@ -2,11 +2,18 @@
 
 include 'config.php';
 
+$cookie=$_COOKIE['fingerprint'];
+
+if (is_null($cookie) == 1) {
+    $cookie=bin2hex(random_bytes(5));
+    setcookie('fingerprint',$cookie);
+}
+
 $conn = new mysqli($host, $username, $password, $database);
 
-$session = 'g'.bin2hex(random_bytes(3));
+$session='g'.bin2hex(random_bytes(3));
 
-$sql = "insert into tbl_session ( session ) values ( '".$session."')";
+$sql = "insert into tbl_session ( fingerprint, session ) values ('".$cookie."','".$session."')";
 $result = $conn->query($sql);
 
 $conn->close();
