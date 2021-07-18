3797bae4680728a346e650a432804673c2f371c8
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-07-18 18:46:35 -0300

    Update tmux_send.sh

--- tmux_send.sh
+++ tmux_send.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-tmux send -t $1 'XXXXXXXXXXXXXXXXXXXXXXXXXX' C-m
+session=($(tmux list-sessions -F "#{session_name}"))
+
+i=0
+
+while [ $i -lt ${#session_name[@]} ]
+do
+
+ if [[ ${session_name[i]} =~ "FENIX_" ]]
+ then
+
+    token=0 ;
+
+    tmux send -t ${session[i]} $token C-m
+    echo "Session" ${session[i]} "autorizada."
+ fi
+
+  ((i++))
+done
