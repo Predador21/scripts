ec0d280e6698f5ceda6bae5c3894f26423aa2a5a
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-07-26 23:03:46 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-session=$1
+session='FENIX_'$1
 
 tmux kill-window -t $session > /dev/null
 
@@ -18,6 +18,11 @@ mysql --login-path=$home/config.cnf fenix << EOF
  insert into tbl_url (session,account,url,status) values ('$session','$2','$link','1'); #STATUS 1 = INICIAL
 
  update tbl_session set status = 2 where account = '$2' ;
+
+#1 = SEM TMUX
+#2 = TMUX ABERTO COM URL CAPTURADA
+#4 = CONCLUIDO
+#9 = SESSION ENCERRADA - TIME OUT
 
 EOF
 
