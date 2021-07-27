58aaa4542efb51be88c50d9df0aa71a73b33d659
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-07-26 23:02:44 -0300

    Update kill_tmux.sh

--- kill_tmux.sh
+++ kill_tmux.sh
@@ -16,6 +16,16 @@ do
  if [[ ${session_name[i]} =~ "FENIX_" ]] && [ ${session_created[i]} -lt $limite ]
  then
     tmux kill-window -t ${session_name[i]} > /dev/null
+
+    session=${session_name[i]/FENIX_/}
+
+    mysql --login-path=$home/config.cnf fenix << EOF
+
+     update tbl_session set status = 9 where session = '$session' and status = 2 ;
+
+EOF
+
+    echo "Session" ${session_name[i]} "finalizada por time-out (9)."
  fi
 
   ((i++))
