da398f8ba6d5fdcf51af91c81e0996c8fd3f8b14
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-07-27 00:14:00 -0300

    Update tmux_send.sh

--- tmux_send.sh
+++ tmux_send.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-session=($(tmux list-sessions -F "#{session_name}"))
+session_name=($(tmux list-sessions -F "#{session_name}"))
 
 i=0
 
 while [ $i -lt ${#session_name[@]} ]
 do
 
- if [[ ${session_name[i]} =~ "FENIX_" ]]
- then
+if [[ ${session_name[i]} =~ "FENIX_" ]]
+then
 
-    token=0 ;
+    #session=${session_name[i]/FENIX_/}
 
-    tmux send -t ${session[i]} $token C-m
-    echo "Session" ${session[i]} "autorizada."
- fi
+    session=${session_name[i]}
+
+    query=$(mysql --login-path=$home/config.cnf fenix -se "select token from tbl_url where status = 1 and id = (select max(id) from tbl_url where session = '$session' >
+
+    read token <<< $query
+
+    if [ "$token" != "NULL" ]
+    then
+
+       tmux send -t ${session_name[i]} $token C-m
+
+       echo "token: "$token
+
+       #UPDATE PARA STATUS 3
+
+       #VERIFICACAO GCLOUD AUTH LIST
+       echo "Session" ${session_name[i]} "autorizada."
+
+   fi
+
+fi
 
   ((i++))
 done
