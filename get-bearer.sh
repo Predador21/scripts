b7ac8a9d9bb054362d8777e79850c4b3d8235467
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-05 12:18:59 -0300

    Create get-bearer.sh

---
+++ get-bearer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ file='.'$(openssl rand -hex 12)
+
+ curl -s --request POST \
+         --url 'https://oauth2.googleapis.com/token' \
+         --header 'content-type: application/x-www-form-urlencoded' \
+         --data grant_type=refresh_token \
+         --data 'client_id=32555940559.apps.googleusercontent.com' \
+         --data client_secret=ZmssLNjJy2998hD4CTg2ejr2 \
+         --data refresh_token=$1 > $file
+
+ token=$(jq '.access_token' $file )
+ rm -rf $file
+
+ echo ${token//'"'/}
