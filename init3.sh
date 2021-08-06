c6bbb2ebdfd9afa1d8722f02c8f5f861dc99bbaf
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-06 01:30:23 -0300

    Create init3.sh

---
+++ init3.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+status1='null'
+token='null'
+
+while true
+do
+
+ echo
+ date
+ echo
+
+ if [ $token == 'null' ] || [ $status1 == 'UNAUTHENTICATED' ]
+ then
+    source get-bearer.sh $1
+ fi
+
+ file='.'$(openssl rand -hex 12)
+
+ curl -s --request POST \
+         --url 'https://cloudshell.googleapis.com/v1/users/me/environments/default:start?alt=json' \
+         --header 'Authorization: Bearer '$token'' \
+         --header 'Accept: application/json' \
+         --header 'Content-Type: application/json' \
+         --compressed > $file
+
+ user=$(jq '.response.environment.sshUsername' $file)
+ status1=$(jq '.error.status' $file)
+ status2=$(jq '.metadata.state' $file)
+ status3=$(jq '.response.environment.state' $file)
+ status4=$(jq '.error.details[0].code' $file)
+
+ user=${user//'"'/}
+
+
+ if [ $user != 'null' ]
+ then
+    user=$user'@gmail.com'
+ fi
+
+ status1=${status1//'"'/} #UNAUTHENTICATED
+ status2=${status2//'"'/} #STARTING/FINISHED
+ status3=${status3//'"'/} #RUNNING
+ status4=${status4//'"'/} #
+
+ echo 'user:    '$user
+ echo 'status1: '$status1
+ echo 'status2: '$status2
+ echo 'status3: '$status3
+ echo 'status4: '$status4
+
+ if [ $status1 != 'null' ]
+ then
+    status=$status1
+ else
+    if [ $status4 != 'null' ]
+    then
+       status=$status4
+    else
+       if [ $status3 != 'null' ]
+       then
+          status=$status3
+       else
+          status=$status2
+       fi
+    fi
+ fi
+
+ #curl
+
+ echo 'status final: '$status
+
+ rm -rf $file
+
+ sleep 60
+
+ done
