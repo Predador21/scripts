ac3d31ffe9174480085ad5ed90c249bc8144e426
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-06 00:13:27 -0300

    Update get-refresh_token.sh

--- get-refresh_token.sh
+++ get-refresh_token.sh
@@ -5,5 +5,6 @@
  (sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$1'") > $file
 
  refresh_token=$(jq '.refresh_token' $file)
+ refresh_token=${refresh_token//'"'/}
 
  rm -rf $file
