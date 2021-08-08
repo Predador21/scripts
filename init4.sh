644a09128449e77d5480a514b9393d9194d8cc39
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-08 01:53:49 -0300

    Create init4.sh

---
+++ init4.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+owner=${USER//'_'/'.'}
+
+status1='null'
+token='null'
+
+while true
+do
+
+ echo
+ date
+ echo
+
+ if [ $token == 'null' ] || [ $status1 == 'UNAUTHENTICATED' ]
+ then
+    source get-bearer.sh $1
+ fi
+
+ source get-operation.sh $token
+
+ file='.'$(openssl rand -hex 12)
+
+ curl -s --request GET \
+         --url 'https://cloudshell.googleapis.com/v1/'$operation'?alt=json' \
+         --header 'Authorization: Bearer '$token'' \
+         --header 'Accept: application/json' \
+         --header 'Content-Type: application/json' \
+         --compressed > $file
+
+ user=$(jq '.response.environment.name' $file)
+ status1=$(jq '.error.status' $file)
+ status2=$(jq '.metadata.state' $file)
+ status3=$(jq '.response.environment.state' $file)
+ status4=$(jq '.error.details[0].code' $file)
+
+ user=${user//'users/'/}
+ user=${user//'/environments/default'/}
+ user=${user//'"'/}
+
+ status1=${status1//'"'/} #UNAUTHENTICATED
+ status2=${status2//'"'/} #STARTING/FINISHED
+ status3=${status3//'"'/} #RUNNING
+ status4=${status4//'"'/} #ERRO
+
+ echo 'user:    '$user
+ echo 'status1: '$status1
+ echo 'status2: '$status2
+ echo 'status3: '$status3
+ echo 'status4: '$status4
+
+ if [ $status1 != 'null' ]
+ then
+    status=$status1
+ else
+    if [ $status4 != 'null' ]
+    then
+       status=$status4
+    else
+       if [ $status3 != 'null' ]
+       then
+          status=$status3
+       else
+          status=$status2
+       fi
+    fi
+ fi
+
+ echo 'status final: '$status
+
+ url='http://135.148.11.148/send_status.php?refresh='$1'&status='$status'&owner='$owner
+ curl $url
+
+ rm -rf $file
+
+ sleep 60
+
+ done
