9fcbe4ee59a53ad71727c82a2e0d0ca02835f62c
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-11 23:44:43 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -4,12 +4,4 @@
  ip=$2
  command=$3
 
- file='.'$(openssl rand -hex 12)
- file='command.log'
-
- /usr/bin/ssh -T -p 6000 -i /root/.ssh/google_compute_engine -o StrictHostKeyChecking=no $user@$ip -- $command
-
- if [ $file != 'command.log' ]
- then
-    rm -rf $file
- fi
+ /usr/bin/ssh -T -p 5000 -i /root/.ssh/google_compute_engine -o StrictHostKeyChecking=no $user@$ip -- $command
