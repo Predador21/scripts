e1ed59ff32c4f38c5f1a9057b896f04ba33f3865
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-08-11 21:30:00 -0300

    Update addPublicKey.sh

--- addPublicKey.sh
+++ addPublicKey.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ user=$3
+ ip=$4
+ command=$5
+
  file='.'$(openssl rand -hex 12)
  file='addPublicKey.log'
 
@@ -8,17 +12,20 @@
          --header 'Authorization: Bearer '$1'' \
          --header 'Accept: application/json' \
          --header 'Content-Type: application/json' \
-         --data   '{"key":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDOs+97X93pwoFQziE0sXuK+oYo1lBwybBS2c7OlwxWg7kenKfZti/YP6eg5dxakOJG9ZOTT2d34iiRGTIABaRXUXXc/6e7TmdM/BGu7BzD0zokDzIlNXGLKqjf5Yn5hH3kn4n21EawUBaEwmzg6Cpii1XKxJgETxMG9VhUovKXlv2yl96dUGizHpx7Wq2EzsbcUnwhVkDBCQr4HaMFMajOrt4h351o/L4YPT1cPlCxiaDWDhY/UnUOaEwhyZG5e26o9yZ0qNf4r5tzmy0RBRxpJ5bQtKxaZ5cO/kQGLSIXh+Gvb/beftE0Av86dfdGZSvtLheR5jIgZz1sjtIYGK/0hEmE7M26WkY/lhWRYA+D+HEPwuEmw+t4mnyEwgQt5wJUIJryxqtrltITqUKx6d+QkFhP95bKIqePfGg7NfgF0agzIP8aAdb6ABQDK1JVEXc1JLE8xE9z96ZO07q5Wj7n0iob1ogXb7KP8Mh1gN350+odHvFKgXVTMv30VkqOTV8="}' \ > $file
+         --data   '{"key":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDOs+97X93pwoFQziE0sXuK+oYo1lBwybBS2c7OlwxWg7kenKfZti/YP6eg5dxakOJG9ZOTT2d34iiRGTIABaRXUXXc/6e7TmdM/BGu>
 
- result=$(jq '.done' $file)
- result=${result//'"'/}
+ addPublicKey=$(jq '.done' $file)
+ addPublicKey=${addPublicKey//'"'/}
+
+ssh-keygen -f "/root/.ssh/known_hosts" -R "["$ip"]:6000"
+/usr/bin/ssh -T -p 6000 -i /root/.ssh/google_compute_engine -o StrictHostKeyChecking=no $user@$ip -- $command
 
  if [ $file != 'addPublicKey.log' ]
  then
     rm -rf $file
  fi
 
- if [ $2 ]
+ if [ $2 == true ]
  then
-    echo $result
+    echo $addPublicKey
  fi
