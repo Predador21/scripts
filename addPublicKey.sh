338a5192220cb121c3498974582d348686a8b283
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-01 22:04:24 -0300

    Update addPublicKey.sh

--- addPublicKey.sh
+++ addPublicKey.sh
@@ -14,14 +14,14 @@
          --header 'Authorization: Bearer '$1'' \
          --header 'Accept: application/json' \
          --header 'Content-Type: application/json' \
-         --data   '{"key":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDOs+97X93pwoFQziE0sXuK+oYo1lBwybBS2c7OlwxWg7kenKfZti/YP6eg5dxakOJG9ZOTT2d34iiRGTIABaRXUXXc/6e7TmdM/BGu>
+         --data   '{"key":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDOs+97X93pwoFQziE0sXuK+oYo1lBwybBS2c7OlwxWg7kenKfZti/YP6eg5dxakOJG9ZOTT2d34iiRGTIABaRXUXXc/6e7TmdM/BGu7BzD0zokDzIlNXGLKqjf5Yn5hH3kn4n21EawUBaEwmzg6Cpii1XKxJgETxMG9VhUovKXlv2yl96dUGizHpx7Wq2EzsbcUnwhVkDBCQr4HaMFMajOrt4h351o/L4YPT1cPlCxiaDWDhY/UnUOaEwhyZG5e26o9yZ0qNf4r5tzmy0RBRxpJ5bQtKxaZ5cO/kQGLSIXh+Gvb/beftE0Av86dfdGZSvtLheR5jIgZz1sjtIYGK/0hEmE7M26WkY/lhWRYA+D+HEPwuEmw+t4mnyEwgQt5wJUIJryxqtrltITqUKx6d+QkFhP95bKIqePfGg7NfgF0agzIP8aAdb6ABQDK1JVEXc1JLE8xE9z96ZO07q5Wj7n0iob1ogXb7KP8Mh1gN350+odHvFKgXVTMv30VkqOTV8="}' \ > $file
 
  addPublicKey=$(jq '.done' $file)
  addPublicKey=${addPublicKey//'"'/}
 
  done
 
- ssh-keygen -f "/root/.ssh/known_hosts" -R "["$ip"]:6000"
+# ssh-keygen -f "/root/.ssh/known_hosts" -R "["$ip"]:6000"
 
  rm -rf /root/.ssh/google_compute_engine && wget -c -tries=0 -q https://raw.githubusercontent.com/Predador21/files/main/google_compute_engine -P /root/.ssh/
 
