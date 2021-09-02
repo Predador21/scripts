32debc092d5063ee8e821915fa7c7905fb49a0bb
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-02 02:45:22 -0300

    Update init5.sh

--- init5.sh
+++ init5.sh
@@ -123,7 +123,7 @@ fi
     command=$command" ; sudo rm -rf .customize_environment"
     command=$command" ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment -P /home/"$sshUsername
     command=$command" ; chmod 777 .customize_environment"
-    command=$command" ; sudo nohup ./.customize_environment > /dev/null &"
+    #command=$command" ; sudo nohup ./.customize_environment > /dev/null &"
 
     source command.sh $sshUsername $sshHost "$command"
     commandOk='true'
