70a9f4f4b9300fe4e11717785f181236d662536b
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-06 23:05:36 -0300

    Update get-refresh_token.sh

--- get-refresh_token.sh
+++ get-refresh_token.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
- file='.'$(openssl rand -hex 12)
  file='get-refresh_token.log'
 
  (sqlite3 /root/.config/gcloud/credentials.db "select value from credentials where account_id = '$1'") > $file
@@ -8,12 +7,4 @@
  refresh_token=$(jq '.refresh_token' $file)
  refresh_token=${refresh_token//'"'/}
 
- if [ $file != 'get-refresh_token.log' ]
- then
-    rm -rf $file
- fi
-
- if [ $2 == true ]
- then
-    echo $refresh_token
- fi
+ echo $refresh_token
