2cd786265f82ceedf509088f981a977e9e688334
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-07 00:33:21 -0300

    Update get-operation.sh

--- get-operation.sh
+++ get-operation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
- file='.'$(openssl rand -hex 12)
- #file='get-operation.log'
+ file='get-operation.log'
 
  curl -s --request POST \
          --url 'https://cloudshell.googleapis.com/v1/users/me/environments/default:start?alt=json' \
@@ -15,8 +14,3 @@
 
  status_operation=$(jq '.error.status' $file)
  status_operation=${status_operation//'"'/}
-
- if [ $file != 'get-operation.log' ]
- then
-    rm -rf $file
- fi
