ac18b8f3d9b2cd372602d235fd22f029d4693f64
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-07 03:46:12 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -4,4 +4,4 @@
  ip=$2
  command=$3
 
- /usr/bin/ssh -T -p 6000 -i google_compute_engine -o StrictHostKeyChecking=no $user@$ip -- $command
+ /usr/bin/ssh -n -T -p 6000 -i google_compute_engine -o StrictHostKeyChecking=no $user@$ip -- $command
