25133f0f7fc4f0093c26d941f34b908d42d24a1d
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-08 01:23:00 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment
+if [ $2 == '0.1' ]
+then
+   rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment
+fi
 
 echo 'upgrade ok'
