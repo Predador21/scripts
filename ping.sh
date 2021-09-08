f0705bc7faf9894d49e1967b7dd1479ae05ea63f
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-07 22:53:40 -0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-while pgrep xmrig > /dev/null
+while true
 do
-   curl http://135.148.11.148/ping.php?account=$1&version=$2
+   if pgrep xmrig > /dev/null
+   then
+       url='http://135.148.11.148/ping.php?account='$1'&version='$2
+       curl $url
+   fi
+   
    sleep 60
 done
