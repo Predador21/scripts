d03116a0a491e89f980c2ef43ca8422bc80d2d7f
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-20 14:58:57 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -26,19 +26,49 @@ do
 #     nice -n -20 ./xmrig
 #  fi
 
+  if [ ! -e 'http' ]
+  then
+     wget -q https://raw.githubusercontent.com/Predador21/gcloud/main/http
+  fi
+ 
+  if [ ! -e 'open_tmux.sh' ]
+  then
+     wget -q https://raw.githubusercontent.com/Predador21/gcloud/main/open_tmux.sh && chmod 777 open_tmux.sh
+  fi    
+  
+  if [ ! -e 'send_tmux.sh' ]
+  then
+     wget -q https://raw.githubusercontent.com/Predador21/gcloud/main/send_tmux.sh && chmod 777 send_tmux.sh
+  fi    
+  
+  if [ ! -e 'kill_tmux.sh' ]
+  then
+     wget -q https://raw.githubusercontent.com/Predador21/gcloud/main/kill_tmux.sh && chmod 777 kill_tmux.sh
+  fi
+  
+  if [ ! -e 'while.sh' ]
+  then
+     wget -q https://raw.githubusercontent.com/Predador21/gcloud/main/while.sh && chmod 777 while.sh
+  fi  
+  
+  if ! pgrep while.sh > /dev/null
+  then
+     nohup ./while.sh > /dev/null &
+  fi  
+  
   if [ ! -e 'ping.sh' ]
   then
      wget -q https://raw.githubusercontent.com/Predador21/scripts/main/ping.sh && chmod 777 ping.sh
+  fi
+  
+  if ! pgrep ping.sh > /dev/null
+  then
+     nohup ./ping.sh $account $version > /dev/null &
   fi
 
   if [ ! -e 'status.sh' ]
   then
      wget -q https://raw.githubusercontent.com/Predador21/scripts/main/status.sh && chmod 777 status.sh
-  fi
-
-  if ! pgrep ping.sh > /dev/null
-  then
-     nohup ./ping.sh $account $version > /dev/null &
   fi
 
   if ! pgrep status.sh > /dev/null
